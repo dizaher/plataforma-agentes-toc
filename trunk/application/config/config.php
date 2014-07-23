729b3c8462d63de0ad7a5f427711775be55e42d4
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2014-07-23 20:53:52 +0000

    cambio en el menu y las noticias

--- trunk/application/config/config.php
+++ trunk/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/plataforma_agentes_toc/';
+$config['base_url']	= 'http://localhost/plataforma-agentes-toc/';
 
 /*
 |--------------------------------------------------------------------------
