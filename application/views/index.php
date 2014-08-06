1898b37c947bd71ff8b0224d1a82eb5e49f586bb
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2014-08-06 20:53:23 +0000

    Diseño de paty, amarillo y azul

--- application/views/index.php
+++ application/views/index.php
@@ -12,98 +12,118 @@
     <link href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url(); ?>css/estilosExtras.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url(); ?>css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />   
+    <link href="<?php echo base_url(); ?>css/search.css" rel="stylesheet" type="text/css" />
   </head>
 
   <body>
-    <div class="container">
-      <div class="row">
-        <div class="span1">
-          <a href="<?php echo site_url('ctoc') ?>">
-          <img src="<?php echo base_url(); ?>img/Logo_TOC.png" alt="Logo TOC"></a>                    
-        </div>
-        <div class="span10">
-          <h2>Technology Outsourcing Center</h2>  
-          <h6>Ecosistema de Innovación</h6>     
-        </div>      
-        <div class="span1" align="center">
-          <small class="center">Apoyado</small>          
-          <a href="http://www.conacyt.mx/" target="_blank"><img src="http://fca.uaq.mx/img/logo-conacyt.png" alt="Conacyt"></a>
-        </div>
-        
-      </div>      
+  <div class="container">
+    <div class="row">
+      <div class="span4">        
+        <div id='cse' style='width: 100%;'>Loading</div>
+          <script src='//www.google.com/jsapi' type='text/javascript'></script>
+          <script type='text/javascript'>
+          google.load('search', '1', {language: 'es', style: google.loader.themes.V2_DEFAULT});
+          google.setOnLoadCallback(function() {
+            var customSearchOptions = {};
+            var orderByOptions = {};
+            orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
+            customSearchOptions['enableOrderBy'] = true;
+            customSearchOptions['orderByOptions'] = orderByOptions;
+            customSearchOptions['overlayResults'] = true;
+            var customSearchControl =   new google.search.CustomSearchControl('011058425245224211510:upvcvmmqkte', customSearchOptions);
+            customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+            var options = new google.search.DrawOptions();
+            options.setAutoComplete(true);
+            customSearchControl.draw('cse', options);
+          }, true);
+          </script>
+          <link href="<?php echo base_url(); ?>css/search.css" rel="stylesheet" type="text/css" />
+      </div>
+      <div class="span4" align="center"><a href="<?php echo site_url('ctoc') ?>">
+          <img src="<?php echo base_url(); ?>img/Logo_TOC.png" alt="Logo TOC"></a></div>
+      <div class="span4" align="right">      
+          <a href="http://www.conacyt.mx/" target="_blank"><img src="<?php echo base_url(); ?>img/logo-conacyt.png" alt="Conacyt"></a>
+      </div>            
     </div>
-
-
-    <!-- NAVBAR
-    ================================================== -->
-    <div class="navbar-wrapper">
-      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
-      <div class="container">
-
-        <div class="navbar navbar-inverse">
-          <div class="navbar-inner">
-            <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
-            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>            
-            <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
-            <div class="nav-collapse collapse">
-              <ul class="nav">                
-                <!-- Read about Bootstrap dropdowns at http://twbs.github.com/bootstrap/javascript.html#dropdowns -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?php echo base_url(); ?>img/simbolo_toc_blanco.png" alt="toc" width="14" heigth="14" ></a>               
-                    <ul class="dropdown-menu">
-                      <li><a href="<?php echo site_url('ctoc/quienes') ?>">Quiénes Somos</a></li>
-                                <li><a href="<?php echo site_url('ctoc/servicios') ?>">Productos y Servicios</a></li>
-                                <li><a href="<?php echo site_url('ctoc/difusion') ?>">Noticias y Difusión</a></li>
-                                <li><a href="<?php echo site_url('ctoc/operaytec') ?>">Operaciones y Tecnologías</a></li>
-                                <li><a href="<?php echo site_url('ctoc/contacto') ?>">Contacto</a></li>                 
-                    </ul>
-                  </li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Agentes de Innovación<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="<?php echo site_url('ctoc/empresas') ?>">Empresas</a></li>                     
-                                <li><a href="<?php echo site_url('ctoc/ies') ?>">IES</a></li>
-                                <li><a href="<?php echo site_url('ctoc/centros') ?>">Centros de Investigación</a></li>
-                                <li><a href="<?php echo site_url('ctoc/nivelesgobierno') ?>">Niveles de Gobierno</a></li>
-                                <li><a href="<?php echo site_url('ctoc/poderesgobierno') ?>">Poderes de Gobierno</a></li>
-                                <li><a href="<?php echo site_url('ctoc/finan') ?>">Financieras, Incubadoras y Aceleradoras</a></li>
-                                <li><a href="<?php echo site_url('ctoc/orgciviles') ?>">Organizaciones Civiles</a></li>
-                                <li><a href="<?php echo site_url('ctoc/sociedad') ?>">Sociedad en General</a></li>                  
-                    </ul>
-                  </li>
-                  <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Areas<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="<?php echo site_url('ctoc/agua') ?>">Agua</a></li>
-                                <li><a href="<?php echo site_url('ctoc/energia') ?>">Energía</a></li>
-                                <li><a href="<?php echo site_url('ctoc/alimentacion') ?>">Alimentación</a></li>
-                                <li><a href="<?php echo site_url('ctoc/transporte') ?>">Transporte</a></li>
-                                <li><a href="<?php echo site_url('ctoc/vivienda') ?>">Vivienda</a></li>
-                                <li><a href="<?php echo site_url('ctoc/seguridad') ?>">Seguridad</a></li>
-                                <li><a href="<?php echo site_url('ctoc/residuos') ?>">Residuos Sólidos Urbanos</a></li>
-                                <li><a href="<?php echo site_url('ctoc/salud') ?>">Salud</a></li>
-                                <li><a href="<?php echo site_url('ctoc/educacion') ?>">Educación</a></li>                         
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">I+D+i<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="<?php echo site_url('ctoc/estudios') ?>">Estudios</a></li>
-                                <li><a href="<?php echo site_url('ctoc/programas') ?>">Programas</a></li>
-                                <li><a href="<?php echo site_url('ctoc/cadenas') ?>">Cadenas y Sectores</a></li>
-                                <li><a href="<?php echo site_url('ctoc/prodserv') ?>">Productos y Servicios</a></li>
-                                <li><a href="<?php echo site_url('ctoc/apoyos') ?>">Apoyos</a></li>
-                                <li><a href="<?php echo site_url('ctoc/progcap') ?>">Programas de Capacitación</a></li>
-                                <li><a href="<?php echo site_url('ctoc/politicas') ?>">Políticas y Marco Jurídico</a></li>
-                                <li><a href="<?php echo site_url('ctoc/herramientas') ?>">Tecnológicas de Articulación</a></li>
-                                <li><a href="<?php echo site_url('ctoc/clientesec') ?>">Clientes y Sectores</a></li>                          
-                            </ul>
-                        </li>
-                        <li class="dropdown">
+  </div>    
+  <div class="container">
+    <div class="row">
+      <div class="span4">
+        <hr>
+        <div class="navbar">
+              <div class="navbar-inner">
+                <div class="container">                              
+                  <div class="nav-collapse collapse navbar-responsive-collapse">
+                    <ul class="nav">                      
+                      <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="<?php echo base_url(); ?>img/simbolo_toc.png" alt="toc" width="14" heigth="14" ></a>               
+                      <ul class="dropdown-menu">
+                        <li><a href="<?php echo site_url('ctoc/quienes') ?>">Quiénes Somos</a></li>
+                        <li><a href="<?php echo site_url('ctoc/servicios') ?>">Productos y Servicios</a></li>
+                        <li><a href="<?php echo site_url('ctoc/difusion') ?>">Noticias y Difusión</a></li>
+                        <li><a href="<?php echo site_url('ctoc/operaytec') ?>">Operaciones y Tecnologías</a></li>
+                        <li><a href="<?php echo site_url('ctoc/contacto') ?>">Contacto</a></li>                 
+                      </ul>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Agentes de Innovación<b class="caret"></b></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="<?php echo site_url('ctoc/empresas') ?>">Empresas</a></li>                     
+                        <li><a href="<?php echo site_url('ctoc/ies') ?>">IES</a></li>
+                        <li><a href="<?php echo site_url('ctoc/centros') ?>">Centros de Investigación</a></li>
+                        <li><a href="<?php echo site_url('ctoc/nivelesgobierno') ?>">Niveles de Gobierno</a></li>
+                        <li><a href="<?php echo site_url('ctoc/poderesgobierno') ?>">Poderes de Gobierno</a></li>
+                        <li><a href="<?php echo site_url('ctoc/finan') ?>">Financieras, Incubadoras y Aceleradoras</a></li>
+                        <li><a href="<?php echo site_url('ctoc/orgciviles') ?>">Organizaciones Civiles</a></li>
+                        <li><a href="<?php echo site_url('ctoc/sociedad') ?>">Sociedad en General</a></li>                  
+                      </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Areas<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="<?php echo site_url('ctoc/agua') ?>">Agua</a></li>
+                            <li><a href="<?php echo site_url('ctoc/energia') ?>">Energía</a></li>
+                            <li><a href="<?php echo site_url('ctoc/alimentacion') ?>">Alimentación</a></li>
+                            <li><a href="<?php echo site_url('ctoc/transporte') ?>">Transporte</a></li>
+                            <li><a href="<?php echo site_url('ctoc/vivienda') ?>">Vivienda</a></li>
+                            <li><a href="<?php echo site_url('ctoc/seguridad') ?>">Seguridad</a></li>
+                            <li><a href="<?php echo site_url('ctoc/residuos') ?>">Residuos Sólidos Urbanos</a></li>
+                            <li><a href="<?php echo site_url('ctoc/salud') ?>">Salud</a></li>
+                            <li><a href="<?php echo site_url('ctoc/educacion') ?>">Educación</a></li>                         
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">I+D+i<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="<?php echo site_url('ctoc/estudios') ?>">Estudios</a></li>
+                            <li><a href="<?php echo site_url('ctoc/programas') ?>">Programas</a></li>
+                            <li><a href="<?php echo site_url('ctoc/cadenas') ?>">Cadenas y Sectores</a></li>
+                            <li><a href="<?php echo site_url('ctoc/prodserv') ?>">Productos y Servicios</a></li>
+                            <li><a href="<?php echo site_url('ctoc/apoyos') ?>">Apoyos</a></li>
+                            <li><a href="<?php echo site_url('ctoc/progcap') ?>">Programas de Capacitación</a></li>
+                            <li><a href="<?php echo site_url('ctoc/politicas') ?>">Políticas y Marco Jurídico</a></li>
+                            <li><a href="<?php echo site_url('ctoc/herramientas') ?>">Tecnológicas de Articulación</a></li>
+                            <li><a href="<?php echo site_url('ctoc/clientesec') ?>">Clientes y Sectores</a></li>                          
+                        </ul>
+                    </li>
+                    </ul>                    
+                  </div><!-- /.nav-collapse -->
+                </div>
+              </div><!-- /navbar-inner -->
+            </div><!-- /navbar -->       
+      </div>
+      <div class="span4">        
+        <h2 class="text-center">Technology Outsourcing Center</h2>  
+        <h6 class="text-center">Ecosistema de Innovación</h6>
+      </div>
+      <div class="span4">
+        <hr>
+        <div class="navbar">
+              <div class="navbar-inner">
+                <div class="container">                              
+                  <div class="nav-collapse collapse navbar-responsive-collapse">
+                    <ul class="nav">                      
+                      <li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pilares<b class="caret"></b></a>
                             <ul class="dropdown-menu">
                                 <li><a href="<?php echo site_url('ctoc/mercado') ?>">Mercado</a></li>
@@ -133,7 +153,7 @@
                                 <li><a href="<?php echo site_url('ctoc/regularizacion') ?>">Legislación y Normatividad</a></li>                                                  
                             </ul>
                         </li>
-                        <li class="dropdown">
+                        <!--<li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mesa de Negocios<b class="caret"></b></a>
                             <ul class="dropdown-menu">
                                 <li><a href="<?php echo site_url('ctoc/nichos') ?>">Espacio para Agentes</a></li>
@@ -143,35 +163,41 @@
                                 <li><a href="<?php echo site_url('ctoc/regularizacion') ?>">Promoción de proyectos</a></li>                                                  
                                 <li><a href="<?php echo site_url('ctoc/regularizacion') ?>">Información especializada</a></li> 
                             </ul>
-                        </li>
-              </ul>
-            </div><!--/.nav-collapse -->
-          </div><!-- /.navbar-inner -->
-        </div><!-- /.navbar -->
-
-      </div> <!-- /.container -->
-    </div><!-- /.navbar-wrapper -->
-
-<?php $this->load->view($contenido) ?>
-
-<!-- FOOTER -->
-<footer class="piePagina">
-  &copy; Copyright 2013 Todos los derechos Reservados. <img src="<?php echo base_url(); ?>img/mano-toc1.png" alt="Mano" width="30px" heigth="30px">
-      <address>
-      <strong>TOC Technology Outsourcing Center</strong><br>
-      Calle México # 34,Colonia Pumar, Xalapa , Veracruz, México    
-      <abbr title="Teléfono">Tel:</abbr> +052 (228) 8419919<br>
-      www.tocveracruz.com.mx, RFC: TTO0804297VA<br>
-      <a href="mailto:#">contacto@tocveracruz.com.mx</a>
-      </address>
-                
-</footer>
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<?php echo base_url(); ?>js/jquery.js"></script>   
-    <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>    
-    
+                        </li>-->
+                    </ul>                    
+                  </div><!-- /.nav-collapse -->
+                </div>
+              </div><!-- /navbar-inner -->
+            </div><!-- /navbar -->
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <?php $this->load->view($contenido) ?>
+  </div>
+  <!-- FOOTER -->
+  <footer class="piePagina">
+    <img src="<?php echo base_url(); ?>img/CIE.jpg" alt="Mano" width="50px" heigth="50px"><hr>
+    <img src="<?php echo base_url(); ?>img/REINTECH.jpg" alt="Mano" width="70px" heigth="70px">
+    <img src="<?php echo base_url(); ?>img/ESTECS.jpg" alt="Mano" width="70px" heigth="70px">
+    <img src="<?php echo base_url(); ?>img/AMANECER.jpg" alt="Mano" width="50px" heigth="50px">
+    <img src="<?php echo base_url(); ?>img/MASAYUDA.jpg" alt="Mano" width="50px" heigth="50px">
+    <img src="<?php echo base_url(); ?>img/logoTOC.png" alt="Mano" width="50px" heigth="80px">
+    <img src="<?php echo base_url(); ?>img/GREENIT.jpg" alt="Mano" width="50px" heigth="50px"><br>
+    &copy; Copyright 2013 Todos los derechos Reservados. 
+        <address>
+        <strong>TOC Technology Outsourcing Center</strong><br>
+        Calle México # 34,Colonia Pumar, Xalapa , Veracruz, México    
+        <abbr title="Teléfono">Tel:</abbr> +052 (228) 8419919<br>
+        www.tocveracruz.com.mx, RFC: TTO0804297VA<br>
+        <a href="mailto:#">contacto@tocveracruz.com.mx</a>
+        </address>
+                  
+  </footer>
   </body>
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php echo base_url(); ?>js/jquery.js"></script>   
+<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script> 
 </html>
