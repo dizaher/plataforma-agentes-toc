6ccdbcd9697a6ad1466d56c284c5ab4cc079cd77
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2014-08-06 20:53:23 +0000

    Diseño de paty, amarillo y azul

--- trunk/application/views/noticias_view.php
+++ trunk/application/views/noticias_view.php
@@ -1,6 +1,51 @@
-
+<div id="myCarousel" class="carousel slide">
+  <div class="carousel-inner">      
+    <div class="item active">
+      <div class="row">
+        <div class="span1"></div>
+        <div class="span5 center">
+          <img src="<?php echo base_url(); ?>img/Noticias/C_1.jpg" alt="" >
+        </div>
+        <div class="span5 carousel-fondo">              
+            <h4>Producto piloto en energía: Calentador Solar</h4>
+            <p>La empresa TOC diseña y construye un calentador solar el cual tiene la función principal de calentar agua con la mayor parte de la energía solar que se pueda captar durante el día. Al desarrollar este producto el objetivo es lograr un ahorro en el consumo de gas, gracias al Calentador Solar se puede mantener el agua caliente durante todo el día sin hacer uso de combustible alguno.</p>             
+            <p><a class="btn" href="<?php echo site_url('reintech_controller/principal_notice') ?>">Ver Nota &raquo;</a></p>
+        </div>
+      </div>              
+    </div>
+    <div class="item">
+      <div class="row">
+        <div class="span1"></div>
+        <div class="span5 center">
+          <img src="<?php echo base_url(); ?>img/Noticias/E_2.jpg" alt="" >
+        </div>
+        <div class="span5 carousel-fondo">              
+            <h4>Producto piloto en energía: Generador Eólico</h4>
+            <p>La empresa TOC crea un Generador Eólico con un diseño único y funcional el cual busca aprovechar al máximo los vientos unidireccionales, predominantes en la zona de Xalapa y Veracruz. Un aspecto importante es que la Empresa TOC con este producto está generando una estrategia para el cuidado del medio ambiente y aprovechar las energías renovables.</p>              
+            <p><a class="btn" href="<?php echo site_url('reintech_controller/principal_notice') ?>">Ver Nota &raquo;</a></p>
+        </div>  
+      </div>                                  
+    </div>   
+    <div class="item">
+      <div class="row">
+        <div class="span1"></div>
+        <div class="span5 center">
+          <img src="<?php echo base_url(); ?>img/Noticias/SAR_1.jpg" alt="">
+        </div>
+        <div class="span5 carousel-fondo">              
+            <h4>Producto piloto en agua: Sistema de Administración de Agua Residencial SAAR</h4>
+            <p>El aumento en la demanda de agua provocado por el crecimiento acelerado de la población, la sobreexplotación de los ríos, arroyos y demás cuerpos de agua superficiales así como el uso irracional del recurso son algunas de las principales causas de la vulnerabilidad y paulatino agotamiento de las reservas.</p>              
+            <p>En este sentido, la importancia de captar, almacenar y utilizar el agua de lluvia para uso doméstico y consumo humano, así como la reutilización de aguas ligeramente contaminadas en actividades que no requieren su potabilización es de gran relevancia para la mayoría de las poblaciones, sobre todo aquellas que no tienen acceso a este vital líquido.</p>
+            <p><a class="btn" href="<?php echo site_url('reintech_controller/principal_notice') ?>">Ver Nota &raquo;</a></p>
+        </div>  
+      </div>                                  
+    </div>
+  </div>
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+</div>
 <!-- Carousel
-    ================================================== -->
+    ================================================== 
     <div id="myCarousel" class="carousel slide">
       <div class="carousel-inner">
         <div class="item active">
@@ -29,14 +74,14 @@
             <div class="carousel-caption">
               <h1>Producto piloto en energía: Motor Stirling  versión 2.0</h1>
               <p class="lead">La empresa TOC Technology Outsorcing Center construyó un motor para bombear y generar electricidad, es un motor Stirling tipo gamma o motor de combustión externa. Este motor genera un trabajo mecánico gracias a la variación de presión provocada por una diferencia de temperaturas. Los motores Stirling funcionan a volumen constante, el gas que se puede ocupar es helio, hidrogeno, nitrógeno, metano y aire, este último es el gas de trabajo que se ocupa en este motor prototipo. </p>
-              <a class=" btn btn-inverse" href="#">Ver detalles</a>
+              <a class="btn btn-inverse" href="#">Ver detalles</a>
             </div>
           </div>
         </div>
       </div>
       <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
       <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-    </div><!-- /.carousel -->
+    </div>--><!-- /.carousel -->
 
 
 
@@ -52,18 +97,19 @@
           <img class="img-circle" data-src="holder.js/140x140">
           <h3>Sistema de monitoreo de productos</h3>
           <p>La empresa TOC en colaboración con un alumno de maestría de ingeniería de software desarrollaron un sistema capaz de monitorear y mostrar reportes de los valores sensados por los productos piloto.</p>
+          <p>El monitoreo se basa en verificar el estado de los productos piloto que se tienen instalados en el hogar, con el fin de observar su funcionamiento.</p>
           <p><a class="btn" href="#">Ver detalles &raquo;</a></p>
         </div><!-- /.span4 -->
         <div class="span4">
           <img class="img-circle" data-src="holder.js/140x140">
           <h3>Sistema acuaponico chinampero</h3>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p>Descubre como la empresa TOC, TECHNOLOGY OUTSOURCING CENTER SA DE CV, ha desarrollado un sistema para dar una solución al tema de alimentos frescos y de calidad al alcance de uno, con un sistema que combina dos técnicas agropecuarias más la inclusión de un método ancestral de los Aztecas, para, la obtención de vegetales y peces, de la cosecha a tú mesa.</p>
           <p><a class="btn" href="#">Ver detalles &raquo;</a></p>
         </div><!-- /.span4 -->
         <div class="span4">
           <img class="img-circle" data-src="holder.js/140x140">
-          <h3>Sistema de administración de agua residencial</h3>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <h3>Motor Stirling  versión 2.0</h3>
+          <p>La empresa TOC construye un motor para bombear y generar electricidad, es un motor Stirling tipo gamma o motor de combustión externa. Este motor genera un trabajo mecánico gracias a la variación de presión provocada por una diferencia de temperaturas. Los motores Stirling funcionan a volumen constante, el gas que se puede ocupar es helio, hidrogeno, nitrógeno, metano y aire, este último es el gas de trabajo que se ocupa en este motor prototipo. </p>
           <p><a class="btn" href="#">Ver detalles &raquo;</a></p>
         </div><!-- /.span4 -->
       </div><!-- /.row -->
