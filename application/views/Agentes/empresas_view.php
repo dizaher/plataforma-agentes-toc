89450edfb9e4cf5794ffd0f3c3f2f50a4a049493
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2014-08-13 21:54:34 +0000

    Se quito un error de un sub-submenu de las empresas_view.php

--- application/views/Agentes/empresas_view.php
+++ application/views/Agentes/empresas_view.php
@@ -10,18 +10,11 @@
 				</ul>
 			</div>
 		</div>
-		<div class="tabbable tabs-left">
-			<ul class="nav nav-tabs">
-				<li class="active"><a href="#1" data-toggle="tab">Section 1</a></li>
-				<li><a href="#2" data-toggle="tab">Section 2</a></li>
-			</ul>
-			<div class="tab-content">
-				<div class="tab-pane active" id="1">
-					<p>I'm in Section 1.</p>
-				</div>
-				<div class="tab-pane" id="2">
-					<p>Howdy, I'm in Section 2.</p>
-				</div>
+		<div class="row">
+			<div class="span12">
+				<center>
+				<img src="<?php echo base_url(); ?>img/LogoRemodelacion.gif" >
+				</center>				
 			</div>
 		</div>
 	</div>		
