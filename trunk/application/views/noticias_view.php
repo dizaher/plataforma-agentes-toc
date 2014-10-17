822c755bb3b2ee757a92442ca6330f3a6ac1eac9
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2014-10-17 18:55:17 +0000

    Queda terminada la pantalla principal, falta modificar las de contenido

--- trunk/application/views/noticias_view.php
+++ trunk/application/views/noticias_view.php
@@ -1,112 +1,87 @@
-<div id="myCarousel" class="carousel slide">
-  <div class="carousel-inner">      
-    <div class="item active">
-      <div class="row">
-        <div class="span1"></div>
-        <div class="span5 center">
-          <img src="<?php echo base_url(); ?>img/Noticias/C_1.jpg" alt="" >
-        </div>
-        <div class="span5 carousel-fondo">              
-            <h4>Producto piloto en energía: Calentador Solar</h4>
-            <p>La empresa TOC diseña y construye un calentador solar el cual tiene la función principal de calentar agua con la mayor parte de la energía solar que se pueda captar durante el día. Al desarrollar este producto el objetivo es lograr un ahorro en el consumo de gas, gracias al Calentador Solar se puede mantener el agua caliente durante todo el día sin hacer uso de combustible alguno.</p>             
-            <p><a class="btn" href="<?php echo site_url('reintech_controller/principal_notice') ?>">Ver Nota &raquo;</a></p>
-        </div>
-      </div>              
+<div class="grid fluid">
+    <div class="row">
+        <div class="carousel bg-transparent" id="carousel1">
+          <div class="slide">
+            <div class="span1"></div>
+            <div class="span5">
+              <div class="notice marker-on-right">
+                <h4>Producto piloto en energía: Calentador Solar</h4>
+                <p>La empresa TOC diseña y construye un calentador solar el cual tiene la función principal de calentar agua con la mayor parte de la energía solar que se pueda captar durante el día. Al desarrollar este producto el objetivo es lograr un ahorro en el consumo de gas, gracias al Calentador Solar se puede mantener el agua caliente durante todo el día sin hacer uso de combustible alguno.</p>                               
+                <p><a class="button" href="#">Ver Nota &raquo;</a></p>                        
+              </div>
+            </div> 
+            <div class="span5">
+              <img src="<?php echo base_url(); ?>img/Noticias/C_1.jpg" alt="" class="cover1" />
+            </div>                                                                                         
+          </div>
+          <div class="slide">
+            <div class="span1"></div>
+            <div class="span5">
+              <div class="notice marker-on-right">
+                <h4>Producto piloto en energía: Generador Eólico</h4>
+                <p>La empresa TOC crea un Generador Eólico con un diseño único y funcional el cual busca aprovechar al máximo los vientos unidireccionales, predominantes en la zona de Xalapa y Veracruz. Un aspecto importante es que la Empresa TOC con este producto está generando una estrategia para el cuidado del medio ambiente y aprovechar las energías renovables.</p>              
+                <p><a class="button" href="#">Ver Nota &raquo;</a></p>
+              </div>
+            </div>
+            <div class="span5">
+              <img src="<?php echo base_url(); ?>img/Noticias/E_2.jpg" alt="" class="cover1" />
+            </div>
+          </div>
+          <div class="slide">
+            <div class="span1"></div>
+            <div class="span5">
+              <div class="notice marker-on-right">
+                <h4>Producto piloto en agua: Sistema de Administración de Agua Residencial SAAR</h4>
+                <p>El aumento en la demanda de agua provocado por el crecimiento acelerado de la población, la sobreexplotación de los ríos, arroyos y demás cuerpos de agua superficiales así como el uso irracional del recurso son algunas de las principales causas de la vulnerabilidad y paulatino agotamiento de las reservas.</p>              
+                <p>En este sentido, la importancia de captar, almacenar y utilizar el agua de lluvia para uso doméstico y consumo humano, así como la reutilización de aguas ligeramente contaminadas en actividades que no requieren su potabilización es de gran relevancia para la mayoría de las poblaciones, sobre todo aquellas que no tienen acceso a este vital líquido.</p>
+                <p><a class="button" href="#">Ver Nota &raquo;</a></p>
+              </div>
+            </div>
+            <div class="span5">
+              <img src="<?php echo base_url(); ?>img/Noticias/SAR_1.jpg" alt="" class="cover1" />
+            </div>
+          </div>
+
+          <a class="controls left"><i class="icon-arrow-left-3"></i></a>
+          <a class="controls right"><i class="icon-arrow-right-3"></i></a>
+      </div>
+      <script>
+          $(function(){
+              $("#carousel1").carousel({
+                  height: 400
+              });
+          })
+      </script>
     </div>
-    <div class="item">
-      <div class="row">
-        <div class="span1"></div>
-        <div class="span5 center">
-          <img src="<?php echo base_url(); ?>img/Noticias/E_2.jpg" alt="" >
-        </div>
-        <div class="span5 carousel-fondo">              
-            <h4>Producto piloto en energía: Generador Eólico</h4>
-            <p>La empresa TOC crea un Generador Eólico con un diseño único y funcional el cual busca aprovechar al máximo los vientos unidireccionales, predominantes en la zona de Xalapa y Veracruz. Un aspecto importante es que la Empresa TOC con este producto está generando una estrategia para el cuidado del medio ambiente y aprovechar las energías renovables.</p>              
-            <p><a class="btn" href="<?php echo site_url('reintech_controller/principal_notice') ?>">Ver Nota &raquo;</a></p>
-        </div>  
-      </div>                                  
-    </div>   
-    <div class="item">
-      <div class="row">
-        <div class="span1"></div>
-        <div class="span5 center">
-          <img src="<?php echo base_url(); ?>img/Noticias/SAR_1.jpg" alt="">
-        </div>
-        <div class="span5 carousel-fondo">              
-            <h4>Producto piloto en agua: Sistema de Administración de Agua Residencial SAAR</h4>
-            <p>El aumento en la demanda de agua provocado por el crecimiento acelerado de la población, la sobreexplotación de los ríos, arroyos y demás cuerpos de agua superficiales así como el uso irracional del recurso son algunas de las principales causas de la vulnerabilidad y paulatino agotamiento de las reservas.</p>              
-            <p>En este sentido, la importancia de captar, almacenar y utilizar el agua de lluvia para uso doméstico y consumo humano, así como la reutilización de aguas ligeramente contaminadas en actividades que no requieren su potabilización es de gran relevancia para la mayoría de las poblaciones, sobre todo aquellas que no tienen acceso a este vital líquido.</p>
-            <p><a class="btn" href="<?php echo site_url('reintech_controller/principal_notice') ?>">Ver Nota &raquo;</a></p>
-        </div>  
-      </div>                                  
-    </div>
-  </div>
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
-</div>
-<!-- Carousel
-    ================================================== 
-    <div id="myCarousel" class="carousel slide">
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="<?php echo base_url(); ?>img/Noticias/C_1.jpg" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Producto piloto en energía: Calentador Solar</h1>
-              <p class="lead">La empresa TOC diseña y construye un calentador solar el cual tiene la función principal de calentar agua con la mayor parte de la energía solar que se pueda captar durante el día. Al desarrollar este producto el objetivo es lograr un ahorro en el consumo de gas, gracias al Calentador Solar se puede mantener el agua caliente durante todo el día sin hacer uso de combustible alguno.</p>
-              <a class=" btn btn-inverse" href="#">Ver detalles</a>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo base_url(); ?>img/Noticias/E_2.jpg" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Producto piloto en energía: Generador Eólico</h1>
-              <p class="lead">La empresa TOC crea un Generador Eólico con un diseño único y funcional el cual busca aprovechar al máximo los vientos unidireccionales, predominantes en la zona de Xalapa y Veracruz. Un aspecto importante es que la Empresa TOC con este producto está generando una estrategia para el cuidado del medio ambiente y aprovechar las energías renovables.</p>
-              <a class=" btn btn-inverse" href="#">Ver detalles</a>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo base_url(); ?>img/Noticias/M_1.jpg" alt="">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Producto piloto en energía: Motor Stirling  versión 2.0</h1>
-              <p class="lead">La empresa TOC Technology Outsorcing Center construyó un motor para bombear y generar electricidad, es un motor Stirling tipo gamma o motor de combustión externa. Este motor genera un trabajo mecánico gracias a la variación de presión provocada por una diferencia de temperaturas. Los motores Stirling funcionan a volumen constante, el gas que se puede ocupar es helio, hidrogeno, nitrógeno, metano y aire, este último es el gas de trabajo que se ocupa en este motor prototipo. </p>
-              <a class="btn btn-inverse" href="#">Ver detalles</a>
-            </div>
-          </div>
+</div>        
+<div class="grid fluid">
+  <div class="row">      
+    <div class="span4">                     
+      <div class="panel no-border">
+        <div class="panel-header fg-dark">Sistema de monitoreo de productos</div>
+        <div class="panel-content fg-dark nlp nrp">                 
+          La empresa TOC en colaboración con un alumno de maestría de ingeniería de software desarrollan un sistema de monitoreo de los valores sensados por los productos piloto. El monitoreo se basa en verificar el estado de los productos piloto que se tienen instalados en el hogar, con el fin de observar su funcionamiento, además se cuentan con técnicas de minería de datos para la recolección.
+          <a class="button" href="#">Ver Nota &raquo;</a></p>
         </div>
       </div>
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-    </div>--><!-- /.carousel -->
-
-
-
-    <!-- Marketing messaging and featurettes
-    ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
-
-    <div class="container marketing">
-
-      <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="span4">          
-          <h3>Sistema de monitoreo de productos</h3>
-          <p>La empresa TOC en colaboración con un alumno de maestría de ingeniería de software desarrollan un sistema de monitoreo de los valores sensados por los productos piloto. El monitoreo se basa en verificar el estado de los productos piloto que se tienen instalados en el hogar, con el fin de observar su funcionamiento, además se cuentan con técnicas de minería de datos para la recolección.</p>
-          <p><a class="btn" href="#">Ver detalles &raquo;</a></p>
-        </div><!-- /.span4 -->
-        <div class="span4">        
-          <h3>Sistema acuaponico chinampero</h3>
-          <p>Descubre como la empresa TOC, TECHNOLOGY OUTSOURCING CENTER SA DE CV, ha desarrollado un sistema para dar una solución al tema de alimentos frescos y de calidad al alcance de uno, con un sistema que combina dos técnicas agropecuarias más la inclusión de un método ancestral de los Aztecas, para, la obtención de vegetales y peces, de la cosecha a tú mesa.</p>
-          <p><a class="btn" href="#">Ver detalles &raquo;</a></p>
-        </div><!-- /.span4 -->
-        <div class="span4">          
-          <h3>Motor Stirling  versión 2.0</h3>
-          <p>La empresa TOC construye un motor para bombear y generar electricidad, es un motor Stirling tipo gamma o motor de combustión externa. Este motor genera un trabajo mecánico gracias a la variación de presión provocada por una diferencia de temperaturas. Los motores Stirling funcionan a volumen constante, el gas que se puede ocupar es helio, hidrogeno, nitrógeno, metano y aire, este último es el gas de trabajo que se ocupa en este motor prototipo. </p>
-          <p><a class="btn" href="#">Ver detalles &raquo;</a></p>
-        </div><!-- /.span4 -->
-      </div><!-- /.row -->
-</div><!-- /.container -->
+    </div>
+    <div class="span4">
+      <div class="panel no-border">
+        <div class="panel-header fg-dark">Sistema acuaponico chinampero</div>
+        <div class="panel-content fg-dark nlp nrp">          
+          Descubre como la empresa TOC, TECHNOLOGY OUTSOURCING CENTER SA DE CV, ha desarrollado un sistema para dar una solución al tema de alimentos frescos y de calidad al alcance de uno, con un sistema que combina dos técnicas agropecuarias más la inclusión de un método ancestral de los Aztecas, para, la obtención de vegetales y peces, de la cosecha a tú mesa.
+          <a class="button" href="#">Ver Nota &raquo;</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="span4">
+      <div class="panel no-border">
+        <div class="panel-header fg-dark">Motor Stirling  versión 2.0</div>
+        <div class="panel-content fg-dark nlp nrp">          
+          La empresa TOC construye un motor para bombear y generar electricidad, es un motor Stirling tipo gamma o motor de combustión externa. Este motor genera un trabajo mecánico gracias a la variación de presión provocada por una diferencia de temperaturas. Los motores Stirling funcionan a volumen constante, el gas que se puede ocupar es helio, hidrogeno, nitrógeno, metano y aire, este último es el gas de trabajo que se ocupa en este motor prototipo. 
+          <a class="button" href="#">Ver Nota &raquo;</a>
+        </div>
+      </div>
+    </div>                             
+  </div>
+</div>
