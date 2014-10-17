208e51365509611b565e11199a54777a84b3bce4
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2014-10-17 16:42:51 +0000

    Se agregaron los estilos de la plantilla metro-ui

--- trunk/application/views/index.php
+++ trunk/application/views/index.php
@@ -135,7 +135,7 @@
                     <li><a href="<?php echo site_url('ctoc/cuentas') ?>">Rendición de Cuentas</a></li>                                                     
                 </ul>
             </li>
-            <li class="dropdown ">
+            <!--<li class="dropdown ">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Necesidades a Satisfacer<b class="caret"></b></a>
                 <ul class="dropdown-menu">
                     <li><a href="<?php echo site_url('ctoc/nichos') ?>">Nichos de Mercado</a></li>
@@ -145,7 +145,7 @@
                     <li><a href="<?php echo site_url('ctoc/regularizacion') ?>">Legislación y Normatividad</a></li>                                                  
                 </ul>
             </li>
-            <!--<li class="dropdown">
+            <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mesa de Negocios<b class="caret"></b></a>
                 <ul class="dropdown-menu">
                     <li><a href="<?php echo site_url('ctoc/nichos') ?>">Espacio para Agentes</a></li>
