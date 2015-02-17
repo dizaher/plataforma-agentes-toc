3b39bc7266030a0f02cbed077e1d5628be0c0a04
zadh11 <zadh11@21afa294-bdbf-3869-524c-6d7ae9fa4819>
2015-02-17 17:47:27 +0000

    Se cambio la estructura de la interfaz por una plantilla ya definida

--- application/views/Principal/contacto_view.php
+++ application/views/Principal/contacto_view.php
@@ -1,46 +1,58 @@
-			           			            			           			            			          
-<div class="grid fluid">	
-	<div class="row">
-		<div class="span12">
-			<ul class="breadcrumb">
-				<li>
-					<a href="<?php echo site_url('ctoc') ?>">Principal</a> <span class="divider">/</span>
-				</li>					
-				<li class="active">Contacto</li>
-			</ul>
-		</div>
-	</div>
-	<div class="row">
-		<div class="span12">
-			    <address>
-			    	<p><strong>Mtro. Jorge Miguel Uscanga Villalba</strong></p>					    
-				    <p>TOC Technology Outsourcing Center SA de CV</p>
-				    <p><a href="mailto:#">juscanga@tocveracruz.com.mx</a> | <a href="mailto:#">juscanga@gmail.com</a></p>					    
+ <section class="title">
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span6">
+                    <h1>Contacto</h1>
+                </div>
+                <div class="span6">
+                    <ul class="breadcrumb pull-right">                    	
+                        <li><a href="<?php echo site_url('ctoc') ?>">Principal</a> <span class="divider">/</span></li>
+                        <li class="active">Contacto</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- / .title -->
+
+
+ <section class="no-margin">
+ 	<iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.0690025519949!2d-96.9097953!3d19.5297556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db321d36e9f15d%3A0x7d47474465fb44e4!2sCalle+M%C3%A9xico+29%2C+Xalapa+Enr%C3%ADquez%2C+Vereda!5e0!3m2!1ses-419!2smx!4v1423761513249"></iframe>
+        
+    </section>
+<section id="about-us" class="container main">
+        <div class="row-fluid">
+            <div class="span4">
+                <div class="box">                    
+                    <h5>Mtro. Jorge Miguel Uscanga Villalba</h5>
+                    <p>Director General</p>
+                    <p><a href="mailto:#">juscanga@tocveracruz.com.mx</a> | <a href="mailto:#">juscanga@gmail.com</a></p>					    
 				    <p>Calle México No. 34, Colonia Pumar, CP 91040</p>
 				    <p>Xalapa, Veracruz, México.</p>					    					    
 				    <abbr title="Phone">Teléfono:</abbr> (228) 8419919
-			    </address>
-			    <hr>
-			    <address>
-			    	<p><strong>Ing. Angela Beatríz Chávez Reyes</strong></p>					    
-				    <p>Vinculación</p>
+                </div>
+            </div>
+            <div class="span4">
+                <div class="box">
+                    <p><img src="images/sample/team4.jpg" alt="" ></p>
+                    <h5>Ing. Angela Beatríz Chávez Reyes</h5>
+                    <p>Vinculación</p>
 				    <p><a href="mailto:#">achavez@tocveracruz.com.mx</a> | <a href="mailto:#">cara0818@gmail.com</a></p>					    
 				    <p>Calle México No. 34, Colonia Pumar, CP 91040</p>
 				    <p>Xalapa, Veracruz, México.</p>					    					    
 				    <abbr title="Phone">Teléfono:</abbr> (228) 8419919
-			    </address>
-				<hr>
-			    <address>
-			    	<p><strong>LC.Yessica Gómez Apodaca</strong></p>					    
-				    <p>Administración Financiera</p>
+                </div>
+            </div>        
+            <div class="span4">
+                <div class="box">
+                    <p><img src="images/sample/team4.jpg" alt="" ></p>
+                    <h5>LC.Yessica Gómez Apodaca</h5>
+                    <p>Administración Financiera</p>
 				    <p><a href="mailto:#">ygomez@tocveracruz.com.mx</a></p>					    
 				    <p>Calle México No. 34, Colonia Pumar, CP 91040</p>
 				    <p>Xalapa, Veracruz, México.</p>					    					    
 				    <abbr title="Phone">Teléfono:</abbr> (228) 8419919
-			    </address>
-			    <br>
+                </div>
+            </div>            
 		</div>
-	</div>	
-</div>
-		
-	
+	</section>
--- application/views/Principal/difusion_view.php
+++ application/views/Principal/difusion_view.php
@@ -1,22 +1,24 @@
-				           			            			           			            			          
-	<div class="grid fluid">	
-		<div class="row">
-			<div class="span12">
-				<ul class="breadcrumb">
-					<li>
-						<a href="<?php echo site_url('ctoc') ?>">Principal</a> <span class="divider">/</span>
-					</li>					
-					<li class="active">Difusión</li>
-				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="span12">
-				<center>
-					<img src="<?php echo base_url(); ?>img/LogoRemodelacion.gif" >
-				</center>
-			</div>
-		</div>	
-		
-	</div>
-		
+<section class="title">
+        <div class="container">
+            <div class="row-fluid">
+                <div class="span6">
+                    <h1>Noticias</h1>
+                </div>
+                <div class="span6">
+                    <ul class="breadcrumb pull-right">                    	
+                        <li><a href="<?php echo site_url('ctoc') ?>">Principal</a> <span class="divider">/</span></li>
+                        <li class="active">Difusión</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- / .title -->   
+
+    <!-- 404 error -->
+    <section id="error" class="container">
+        
+        <img src="<?php echo base_url(); ?>img/LogoRemodelacion.gif" >
+        
+    </section>
+    <!-- /404 error 
